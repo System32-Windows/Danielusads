6557171b5f07323e7c03c5d02680e02a6c9da4e4
System32-Windows <pango.land@outlook.com>
2024-04-17 19:37:05 +0200

    com123

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-var currentad = undefined
+var currentad = -1
+var user = -1
 var ads = {
     0: ["miejsce.png", "Miejsce na reklamę", "https://system32-windows.github.io/danielusads"],
 }
 
 var users = {
-    0: ["test", 0]
+    0: ["test@test.test", "test", 0]
 }
 
-function submit() {
+function send() {
+    for (let i = 0; i < 2; i++) {
+        if (document.getElementById("email").value === users[i][0]) {
+            if (document.getElementById("pass").value === users[i][1]) {
+                user = i;
+            } else {
+                document.getElementById("warn").innerHTML = "Nieprawidłowe hasło";
+            }
+        } else {
+            document.getElementById("warn").innerHTML = "Nieprawidłowy email";
+        }
+    }
 
 }
 
